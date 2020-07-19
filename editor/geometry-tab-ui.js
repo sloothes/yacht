54084ab83551a7ea90e592130f0637a44dac4f62
sloothes <sloothes.com@gmail.com>
2020-07-19 03:25:35 +0300

    geometry translate/rotate/scale ui

--- editor/geometry-tab-ui.js
+++ editor/geometry-tab-ui.js
@@ -44,6 +44,160 @@
 
 	})( TabUI.Geometry.tab );
 
+//	vector-droplist-ui.js
+
+	(function( tab ){
+
+	//	Vector mode droplist.
+	//	When option is selected, switches to EditMode.
+	//	var tab = TabUI.Geometry.tab;
+
+		var row = document.createElement("h3");
+		row.textContent = "select:";
+		row.style.cssText = "height:40px;"
+
+		var select = document.createElement("select");
+		select.id = "geometry-vector-droplist";
+		select.style.cssText = "width:170px;color:#000;float:right;"
+		+ "border:1px solid;border-radius:4px;padding:2px 4px 4px 4px;"
+		+ "font-size:20px;margin-left:10px;margin-right:15px;";
+
+		var modes = "translate,rotate,scale";
+		modes.split(",").forEach(function( name ){
+			var option = document.createElement("option");
+			option.text = name;
+			option.value = name;
+			select.appendChild( option );
+		});
+
+		row.appendChild( select );
+		tab.appendChild( row );
+
+	})( TabUI.Geometry.tab );
+
+//	vector-x-ui.js
+
+	(function( tab ){
+
+	//	vector x.
+	//	var tab = TabUI.Geometry.tab;
+
+		var row = document.createElement("h3");
+		row.textContent = "axis x:";
+		row.style.cssText = "margin:10px 15px;height:40px;";
+
+		var vect = document.createElement("div");
+		vect.style.cssText = "width:170px;height:40px;float:right;";
+
+		var prev = document.createElement("li");
+		prev.id = "geometry-vector-x-decrease";
+		prev.innerHTML = "&#9668;";
+		prev.style.display = "inline";
+		prev.classList.add("btn","btn-primary","get-prev-btn","pull-left");
+
+		var next = document.createElement("li");
+		next.id = "geometry-vector-x-increase";
+		next.innerHTML = "&#9658;";
+		next.style.display = "inline";
+		next.classList.add("btn","btn-primary","get-next-btn","pull-right");
+
+		var input = document.createElement("input");
+		input.id = "geometry-vector-x-input";
+		input.setAttribute("placeholder", "x" );
+		input.classList.add("form-control","text-center");
+		input.style.cssText = "color:#000;border:none;display:inline;width:80px;"
+		+ "margin:0px 5px;text-align:center;font-size:large;font-weigth:bold;background:none;";
+
+		vect.appendChild(prev);
+		vect.appendChild(input);
+		vect.appendChild(next);
+		row.appendChild(vect);
+		tab.appendChild( row );
+
+	})( TabUI.Geometry.tab );
+
+//	vector-y-ui.js
+
+	(function( tab ){
+
+	//	vector y.
+	//	var tab = TabUI.Geometry.tab;
+
+		var row = document.createElement("h3");
+		row.textContent = "axis y:";
+		row.style.cssText = "margin:10px 15px;height:40px;";
+
+		var vect = document.createElement("div");
+		vect.style.cssText = "width:170px;height:40px;float:right;";
+
+		var prev = document.createElement("li");
+		prev.id = "geometry-vector-y-decrease";
+		prev.innerHTML = "&#9668;";
+		prev.style.display = "inline";
+		prev.classList.add("btn","btn-primary","get-prev-btn","pull-left");
+
+		var next = document.createElement("li");
+		next.id = "geometry-vector-y-increase";
+		next.innerHTML = "&#9658;";
+		next.style.display = "inline";
+		next.classList.add("btn","btn-primary","get-next-btn","pull-right");
+
+		var input = document.createElement("input");
+		input.id = "geometry-vector-y-input";
+		input.setAttribute("placeholder", "y" );
+		input.classList.add("form-control","text-center");
+		input.style.cssText = "color:#000;border:none;display:inline;width:80px;"
+		+ "margin:0px 5px;text-align:center;font-size:large;font-weigth:bold;background:none;";
+
+		vect.appendChild(prev);
+		vect.appendChild(input);
+		vect.appendChild(next);
+		row.appendChild(vect);
+		tab.appendChild( row );
+
+	})( TabUI.Geometry.tab );
+
+//	vector-z-ui.js
+
+	(function( tab ){
+
+	//	vector z.
+	//	var tab = TabUI.Geometry.tab;
+
+		var row = document.createElement("h3");
+		row.textContent = "axis z:";
+		row.style.cssText = "margin:10px 15px;height:40px;";
+
+		var vect = document.createElement("div");
+		vect.style.cssText = "width:170px;height:40px;float:right;";
+
+		var prev = document.createElement("li");
+		prev.id = "geometry-vector-z-decrease";
+		prev.innerHTML = "&#9668;";
+		prev.style.display = "inline";
+		prev.classList.add("btn","btn-primary","get-prev-btn","pull-left");
+
+		var next = document.createElement("li");
+		next.id = "geometry-vector-z-increase";
+		next.innerHTML = "&#9658;";
+		next.style.display = "inline";
+		next.classList.add("btn","btn-primary","get-next-btn","pull-right");
+
+		var input = document.createElement("input");
+		input.id = "geometry-vector-z-input";
+		input.setAttribute("placeholder", "z" );
+		input.classList.add("form-control","text-center");
+		input.style.cssText = "color:#000;border:none;display:inline;width:80px;"
+		+ "margin:0px 5px;text-align:center;font-size:large;font-weigth:bold;background:none;";
+
+		vect.appendChild(prev);
+		vect.appendChild(input);
+		vect.appendChild(next);
+		row.appendChild(vect);
+		tab.appendChild( row );
+
+	})( TabUI.Geometry.tab );
+
 //	create-geometry-ui.js
 
 	(function( tab ){
